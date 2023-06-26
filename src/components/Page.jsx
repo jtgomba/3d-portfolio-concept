d3ab0f76df7ec0b6661971cab0097b509b109ded
jtgomba <119640744+JoelGomba@users.noreply.github.com>
2023-06-27 01:48:13 +0900

    ?

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,11 +1,132 @@
-/* eslint-disable react/no-unescaped-entities */
-import { Scroll } from "@react-three/drei";
+import { useRef, useLayoutEffect, useEffect, useState } from "react"
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ASScroll from "@ashthornton/asscroll";
+
+gsap.registerPlugin(ScrollTrigger);
+
+function mapRange(value, inputMin, inputMax, outputMin, outputMax) {
+    return ((value - inputMin) * (outputMax - outputMin)) / (inputMax - inputMin) + outputMin;
+}
 
 const Page = () => {
+    const pageWrapperRef = useRef();
+    const pageRef = useRef();
+
+    useEffect(() => {
+        const asscroll = new ASScroll({
+            ease: 0.1,
+            disableRaf: true,
+        });
+
+        gsap.ticker.add(asscroll.update);
+
+        ScrollTrigger.defaults({
+            scroller: asscroll.containerElement,
+        });
+
+        ScrollTrigger.scrollerProxy(asscroll.containerElement, {
+            scrollTop(value) {
+                if (arguments.length) {
+                    asscroll.currentPos = value;
+                    return;
+                }
+                return asscroll.currentPos;
+            },
+            getBoundingClientRect() {
+                return {
+                    top: 0,
+                    left: 0,
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                };
+            },
+            fixedMarkers: true,
+        });
+
+        asscroll.on("update", () => { ScrollTrigger.update });
+        ScrollTrigger.addEventListener("refresh", asscroll.resize);
+
+        requestAnimationFrame(() => {
+            asscroll.enable({
+                newScrollElements: document.querySelectorAll(
+                    ".gsap-marker-start, .gsap-marker-end, [asscroll]"
+                ),
+            });
+        });
+    }, [])
+
+
+
+    useLayoutEffect(() => {
+        const ctx = gsap.context((self) => {
+            const sections = self.selector('.section');
+            sections.forEach((section) => {
+                const thisSection = gsap.utils.selector(section);
+                const progressWrapper = thisSection(".progress-wrapper");
+                const progressBar = thisSection(".progress-bar");
+                if (section.classList.contains("right")) {
+                    gsap.to(section, {
+                        borderTopLeftRadius: 10,
+                        scrollTrigger: {
+                            trigger: section,
+                            start: "top bottom",
+                            end: "top top",
+                            scrub: 0.6,
+                        },
+                    });
+                    gsap.to(section, {
+                        borderBottomLeftRadius: 700,
+                        scrollTrigger: {
+                            trigger: section,
+                            start: "bottom bottom",
+                            end: "bottom top",
+                            scrub: 0.6,
+                        },
+                    });
+                } else {
+                    gsap.to(section, {
+                        'border-top-right-radius': 10,
+                        scrollTrigger: {
+                            trigger: section,
+                            start: 'top bottom',
+                            end: "top top",
+                            scrub: 0.6,
+                        },
+                    });
+                    gsap.to(section, {
+                        'border-bottom-right-radius': 700,
+                        scrollTrigger: {
+                            trigger: section,
+                            start: "bottom bottom",
+                            end: "bottom top",
+                            scrub: 0.6,
+                        },
+                    });
+                }
+                gsap.from(progressBar, {
+                    scaleY: 0,
+                    scrollTrigger: {
+                        trigger: section,
+                        start: "top top",
+                        end: "bottom bottom",
+                        scrub: 0.4,
+                        pin: progressWrapper,
+                        pinSpacing: false,
+                    },
+                });
+            });
+        }, pageWrapperRef); // <- Scope!
+        return () => ctx.revert(); // <- Cleanup!
+    }, []);
+
     return (
-        <Scroll html>
-            <div className="light-theme">
-                <div className="page">
+        <>
+            <div asscroll-container="true"
+                className="page"
+                ref={pageRef}
+                style={{ contain: "content" }}>
+                <div asscroll="true" className="page-wrapper" ref={pageWrapperRef}>
                     <div className="toggle-bar">
                         <div className="sun-wrapper">
                             <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24">
@@ -23,62 +144,120 @@ const Page = () => {
                             </svg>
                         </div>
                     </div>
-                    <div className="page-wrapper">
-                        <section className="hero">
-                            <div className="hero-wrapper">
 
-                                {/* <!-- Intro Stuff --> */}
-                                <div className="intro-text">Welcome to my portfolio!</div>
-                                <div className="arrow-svg-wrapper">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24">
-                                        <path fill="currentColor"
-                                            d="M12 14.95q-.2 0-.375-.063-.175-.062-.325-.212L6.675 10.05q-.275-.275-.262-.688.012-.412.287-.687.275-.275.7-.275.425 0 .7.275l3.9 3.9 3.925-3.925q.275-.275.688-.263.412.013.687.288.275.275.275.7 0 .425-.275.7l-4.6 4.6q-.15.15-.325.212-.175.063-.375.063Z" />
-                                    </svg>
-                                </div>
+                    <section className="hero">
+                        <div className="hero-wrapper">
 
-                                <div className="hero-main">
-                                    <h1 className="hero-main-title">Abigail Bloom</h1>
-                                    <p className="hero-main-description">Digital Media Student | 3D Artist</p>
-                                </div>
-
-                                <div className="hero-second">
-                                    <p className="hero-second-subheading first-sub">AbigailBloom</p>
-                                    <p className="hero-second-subheading second-sub">Portfolio</p>
-                                </div>
-
-                            </div>
-                        </section>
-
-                        <div className="first-move section-margin"></div>
-
-                        <section className="first-section section left">
-                            <div className="progress-wrapper progress-bar-wrapper-left">
-                                <div className="progress-bar"></div>
+                            {/* <!-- Intro Stuff --> */}
+                            <div className="intro-text">Welcome to my portfolio!</div>
+                            <div className="arrow-svg-wrapper">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24">
+                                    <path fill="currentColor"
+                                        d="M12 14.95q-.2 0-.375-.063-.175-.062-.325-.212L6.675 10.05q-.275-.275-.262-.688.012-.412.287-.687.275-.275.7-.275.425 0 .7.275l3.9 3.9 3.925-3.925q.275-.275.688-.263.412.013.687.288.275.275.275.7 0 .425-.275.7l-4.6 4.6q-.15.15-.325.212-.175.063-.375.063Z" />
+                                </svg>
                             </div>
 
-                            <div className="section-intro-wrapper">
-                                <h1 className="section-title">
-                                    <span className="section-title-text">About Me</span>
-                                    <div className="section-title-decoration styleOne"></div>
-                                    <div className="section-title-decoration styleTwo"></div>
-                                    <div className="section-title-decoration styleThree"></div>
-                                </h1>
-                                <span className="section-number">01</span>
+                            <div className="hero-main">
+                                <h1 className="hero-main-title">Joel Gomba</h1>
+                                <p className="hero-main-description">Full-Stack Developer | Specializes in Frontend</p>
                             </div>
 
-
-                            <div className="section-detail-wrapper">
-                                <p className="section-text">Hi there 👋! I'm a third-year digital media student from UK currently studying in
-                                    Germany. My dream is to work for Disney or Pixar one day.</p>
-                                <p className="section-text"> I love creating art and playing with my cats! I also like drinking bubble tea and
-                                    going for hikes! Totally hippie lol ✌️. Welcome to my portfolio!</p>
+                            <div className="hero-second">
+                                <p className="hero-second-subheading first-sub">Joel Gomba</p>
+                                <p className="hero-second-subheading second-sub">Portfolio</p>
                             </div>
-                        </section>
 
-                    </div>
+                        </div>
+                    </section>
+
+                    <div className="first-move section-margin"></div>
+
+                    <section className="first-section section left">
+                        <div className="progress-wrapper progress-bar-wrapper-left">
+                            <div className="progress-bar"></div>
+                        </div>
+
+                        <div className="section-intro-wrapper">
+                            <h1 className="section-title">
+                                <span className="section-title-text">About Me</span>
+                                <div className="section-title-decoration styleOne"></div>
+                                <div className="section-title-decoration styleTwo"></div>
+                                <div className="section-title-decoration styleThree"></div>
+                            </h1>
+                            <span className="section-number">01</span>
+                        </div>
+
+
+                        <div className="section-detail-wrapper">
+                            <p className="section-text">Hi there 👋! I&apos;m a third-year digital media student from UK currently studying in
+                                Germany. My dream is to work for Disney or Pixar one day.</p>
+                            <p className="section-text"> I love creating art and playing with my cats! I also like drinking bubble tea and
+                                going for hikes! Totally hippie lol ✌️. Welcome to my portfolio!</p>
+                        </div>
+                    </section>
+
+                    <div className="second-move section-margin"></div>
+
+                    <section className="second-section section right">
+                        <div className="progress-wrapper progress-bar-wrapper-right">
+                            <div className="progress-bar blue-background"></div>
+                        </div>
+
+                        <div className="section-intro-wrapper blue-text blue-border">
+                            <h1 className="section-title blue-text blue-border">
+                                <span className="section-title-text blue-text">My Work</span>
+                                <div className="section-title-decoration styleOne blue-border"></div>
+                                <div className="section-title-decoration styleTwo blue-border"></div>
+                                <div className="section-title-decoration styleThree blue-background blue-border"></div>
+                            </h1>
+                            <span className="section-number blue-text">02</span>
+                        </div>
+
+                        <div className="section-detail-wrapper">
+                            <h3 className="section-heading">Candycane Village</h3>
+                            <p className="section-text">This project is in progress but it&apos;s about a super colorful village where the entire
+                                world including the people are candies. So far the story is that they are set out to explore their &quot;space&quot;
+                                only to realize it&apos;s a human that will try to destroy them.</p>
+                            <h3 className="section-heading">Rebecca&apos;s Reddish Radishes</h3>
+                            <p className="section-text">Oh what&apos;s that? Why, it&apos;s a red radish! Oop, another one! In this playful and comedy
+                                animation, Rebecca, a young farmer, decided to plant radishes for the first time, but there is a big twist!
+                            </p>
+                            <h3 className="section-heading">Flora</h3>
+                            <p className="section-text">A heartwarming story about a little orphan girl who tries to find her way back home.
+                            </p>
+                        </div>
+                    </section>
+
+                    <div className="third-move section-margin"></div>
+
+                    <section className="third-section section left">
+                        <div className="progress-wrapper progress-bar-wrapper-left">
+                            <div className="progress-bar green-background"></div>
+                        </div>
+
+                        <div className="section-intro-wrapper green-text green-border">
+                            <h1 className="section-title green-text green-border">
+                                <span className="section-title-text green-text">Contact Me</span>
+                                <div className="section-title-decoration styleOne green-border"></div>
+                                <div className="section-title-decoration styleTwo green-border"></div>
+                                <div className="section-title-decoration styleThree green-background green-border"></div>
+                            </h1>
+                            <span className="section-number green-text">03</span>
+                        </div>
+
+                        <div className="section-detail-wrapper">
+                            <h3 className="section-heading">ArtStation</h3>
+                            <p className="section-text">I post all my work here. I don&apos;t want to link it yet because I want to sort it out a
+                                little bit!</p>
+                            <h3 className="section-heading">Instagram</h3>
+                            <p className="section-text">Check out my personal instagram for travel pics and food and stuff.</p>
+                            <h3 className="section-heading">LinkedIn</h3>
+                            <p className="section-text">Career updates and so much more!</p>
+                        </div>
+                    </section>
                 </div>
             </div>
-        </Scroll>
+        </>
     )
 }
 
